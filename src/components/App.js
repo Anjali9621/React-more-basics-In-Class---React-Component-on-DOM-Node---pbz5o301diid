db76b8d2813a0c7d60d840c4cecdd610ad960e87
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-02 21:55:03 +0530

    str

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,2 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-ReactDOM.render(element, container[, callback])
 expert default App;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import App from "./components/App";
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
