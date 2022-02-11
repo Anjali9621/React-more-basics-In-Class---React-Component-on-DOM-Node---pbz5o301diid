349023b83cd32d1be762e811deca3837255534ea
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-11 22:27:38 +0530

    str3

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,9 @@
 import React from "react";
+
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const element = <Welcome name="Sara" />;
+ReactDOM.render(element, document.getElementById("root"));
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
 
-const element = <Welcome name="Sara" />;
 ReactDOM.render(element, document.getElementById("root"));
